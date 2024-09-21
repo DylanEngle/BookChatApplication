ce80097db2861d513c5f18f344e59dc48d53a0d6
DylanEngle <104050134+DylanEngle@users.noreply.github.com>
2024-09-21 18:25:30 -0400

    database configured

--- bookbackend/bookbackend/src/main/java/dev/dylan/bookbackend/BookbackendApplication.java
+++ bookbackend/bookbackend/src/main/java/dev/dylan/bookbackend/BookbackendApplication.java
@@ -2,12 +2,23 @@ package dev.dylan.bookbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class BookbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookbackendApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String getHomePage() {
+		return "Hello World";
+	}
+	
 
 }
